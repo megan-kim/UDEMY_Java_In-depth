1e9353b815e7fee5f86bb237c275fd140b77f24d
megan-kim <megan.chinian@gmail.com>
2017-11-08 00:16:24 +0200

    Hello World
    First Program in Java "Hello World"

---
+++ src/com/semanticsquare/basics/HelloWorld.java
@@ -0,0 +1,15 @@
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
+
+class GoodbyeWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Goodbye World");
+	}
+}
